a83b76ecaaccfcbcc59a65940fa439bcf40639e0
Gabriel Gigante <gabrielgigante29@gmail.com>
2025-03-05 23:40:34 -0300

    Feat/sign in page layout (#3)
    * chore: creates auth cookie name constant
    
    * feat: setup tailwind and shadcn
    
    * feat: adds shadcn component
    
    * feat: sign-in page
    
    * feat: creates dashboard placeholder page
    
    * feat: creates sidebar component

---
+++ src/components/sidebar/index.tsx
@@ -0,0 +1,11 @@
+import { SidebarRoot } from "./sidebar-root";
+import { SidebarHeader } from "./sidebar-header";
+import { SidebarContent } from "./sidebar-content";
+import { SidebarFooter } from "./sidebar-footer";
+
+export const Sidebar = {
+  Root: SidebarRoot,
+  Header: SidebarHeader,
+  Content: SidebarContent,
+  Footer: SidebarFooter,
+};
