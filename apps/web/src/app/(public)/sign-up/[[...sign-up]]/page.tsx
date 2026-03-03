0012719afe03388705dbd7e7fa5d2ed70df6a1e9
Gabriel Gigante <gabrielgigante29@gmail.com>
2026-03-02 23:40:06 -0300

    chore: drop sign-up page

--- apps/web/src/app/(public)/sign-up/[[...sign-up]]/page.tsx
+++
@@ -1,9 +0,0 @@
-import { SignUp } from '@clerk/nextjs'
-
-export default function SignUpPage() {
-  return (
-    <div className="flex min-h-svh items-center justify-center p-6 md:p-10">
-      <SignUp signInUrl="/sign-in" forceRedirectUrl="/" />
-    </div>
-  )
-}
