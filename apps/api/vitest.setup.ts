220b0243542b927866490781fafe2691e32a6c9d
Gabriel Gigante <gabrielgigante29@gmail.com>
2026-03-04 21:12:45 -0300

    chore: adds envs for testS

---
+++ apps/api/vitest.setup.ts
@@ -0,0 +1,6 @@
+process.env.DATABASE_HOST ??= "localhost"
+process.env.DATABASE_PORT ??= "5432"
+process.env.DATABASE_NAME ??= "odonto"
+process.env.DATABASE_USER ??= "postgres"
+process.env.DATABASE_PASSWORD ??= "postgres"
+process.env.CLERK_SECRET_KEY ??= "sk_test_123"
